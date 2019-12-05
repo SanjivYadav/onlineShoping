0f8797e894cfcd410bb1c1541b4f2e894e4c8a2a
Sanjiv Yadav <sanjiv2009mca@gmail.com>
2019-12-05 11:56:51 +0530

    Project setup completed successfully.

---
+++ onlineproductbackend/src/main/java/net/app/onlineproductbackend/App.java
@@ -0,0 +1,13 @@
+package net.app.onlineproductbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
